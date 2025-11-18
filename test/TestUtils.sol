f221f18f93ebce01ccb36a5a4b168b89861bd88b
Daniel Kraft <d@domob.eu>
2025-11-18 19:37:03 +0100

    Use forge for MovePermissions unit tests.
    Migrate the MovePermissions unit tests from Truffle JS to forge.

--- test/TestUtils.sol
+++ test/TestUtils.sol
@@ -3,8 +3,6 @@
 
 pragma solidity ^0.8.13;
 
-/* Construct arrays of 0-4 members easily.  */
-
 function stringArray () pure returns (string[] memory)
 {
   return new string[] (0);
@@ -43,4 +41,66 @@ function stringArray (string memory a, string memory b, string memory c,
   res[1] = b;
   res[2] = c;
   res[3] = d;
+}
+
+function stringArray (string memory a, string memory b, string memory c,
+                      string memory d, string memory e)
+    pure returns (string[] memory res)
+{
+  res = new string[] (5);
+  res[0] = a;
+  res[1] = b;
+  res[2] = c;
+  res[3] = d;
+  res[4] = e;
+}
+
+function addressArray () pure returns (address[] memory)
+{
+  return new address[] (0);
+}
+
+function addressArray (address a)
+    pure returns (address[] memory res)
+{
+  res = new address[] (1);
+  res[0] = a;
+}
+
+function addressArray (address a, address b)
+    pure returns (address[] memory res)
+{
+  res = new address[] (2);
+  res[0] = a;
+  res[1] = b;
+}
+
+function addressArray (address a, address b, address c)
+    pure returns (address[] memory res)
+{
+  res = new address[] (3);
+  res[0] = a;
+  res[1] = b;
+  res[2] = c;
+}
+
+function addressArray (address a, address b, address c, address d)
+    pure returns (address[] memory res)
+{
+  res = new address[] (4);
+  res[0] = a;
+  res[1] = b;
+  res[2] = c;
+  res[3] = d;
+}
+
+function addressArray (address a, address b, address c, address d, address e)
+    pure returns (address[] memory res)
+{
+  res = new address[] (5);
+  res[0] = a;
+  res[1] = b;
+  res[2] = c;
+  res[3] = d;
+  res[4] = e;
 }
